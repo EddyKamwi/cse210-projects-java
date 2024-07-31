ca943eb424380743520341119819ab924f83e609
Edward Kamwi <edwardkamwi@gmail.com>
2024-07-31 05:11:40 +0200

    More creativity mostly in the Program class

--- prove/develop1/Journal.java
+++ prove/develop1/Journal.java
@@ -1,13 +1,13 @@
-import java.util.Date;
 
 class Journal {
-    private Date _date = new Date();
+    private String _date; ;
     private String _prompt;
     private String _promptAns;
 
-    public Journal(String prompt,String answer){
+    public Journal(String prompt,String answer,String date){
         this._prompt = prompt;
         this._promptAns = answer;
+        this._date = date;
     }
     public String GetJournalDisplay(){
         String entery = "Date :"+this._date.toString()+" - Prompt: "+ this._prompt+"\n"+this._promptAns;
--- prove/develop1/Program.java
+++ prove/develop1/Program.java
@@ -1,42 +1,61 @@
+import java.util.*;
+
 public class Program {
 
     public static void Print(String string) {
         System.out.println(string);
     }
 
+    public static void Print(Integer string) {
+        System.out.println(string);
+    }
+
     public static void main(String[] args) {
+        Map<Integer, String[]> dbMap = new HashMap<>();
         String[] prompts = {
-            "Who was the most interesting person I interacted with today?",
-            "What was the best part of my day?",
-            "How did I see the hand of the Lord in my life today?",
-            "What was the strongest emotion I felt today?",
-            "If I had one thing I could do over today, what would it be?",
-            "Did you minister to any one today? Who is in the most need to be ministered to?",
-            "How often did I feel the Savior's love during the day?",
-            "What was one thing I did to prepare myself to partake of the sacrament?",
-            "Are you happy? Why?"};
+                "Who was the most interesting person I interacted with today?",
+                "What was the best part of my day?",
+                "How did I see the hand of the Lord in my life today?",
+                "What was the strongest emotion I felt today?",
+                "If I had one thing I could do over today, what would it be?",
+                "Did you minister to any one today? Who is in the most need to be ministered to?",
+                "How often did I feel the Savior's love during the day?",
+                "What was one thing I did to prepare myself to partake of the sacrament?",
+                "Are you happy? Why?" };
 
         while (true) {
-            System.console();
-            String menu = "1. Write\n2. Display\n3. Load\n4. Save\n5. Quit";
-            Print(menu);
-            try {
-                // employ input implementation for the answer variable
-                String answer = System.console().readLine();
-                Print(answer);
-                if (answer == "1") {
-                    Print("answer is 1");
-                    break;
-                } else if (answer == "2") {
-                    Journal journal = new Journal("??", "Edward");
-                    Print(journal.GetJournalDisplay());
-                }
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
+            Scanner scanner = new Scanner(System.in);
 
-                e.printStackTrace();
+            Print("Welcome to the Journal Program");
+            String menu = "Please select one of the following options:\n1. Write\n2. Display\n3. Load\n4. Save\n5. Quit";
+            Print(menu);
+
+            // employ input implementation for the answer variable
+            Integer answer = scanner.nextInt();
+
+            // Write to journal
+            if (answer == 1) {
+                
             }
 
+            // Display
+            else if (answer == 2) {
+                for (Map.Entry<Integer, String[]> entry : dbMap.entrySet()) {
+                    Journal journal = new Journal(entry.getValue()[0], entry.getValue()[1],entry.getValue()[2]);
+                    Print(journal.GetJournalDisplay());
+                }
+            }
+
+            // Load
+
+            // Save
+
+            // Quict
+            else if (answer == 5) {
+                break;
+            }
+
+            scanner.close();
         }
 
     }
